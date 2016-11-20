3bf768effef038bfd4e2aa0d79757df1fff846b3
msorokin <mi@>
2016-11-20 22:38:36 +0300

    task 2a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+
+  const a = (req.query.a === undefined) ? 0: parseInt(req.query.a);
+  const b = (req.query.b === undefined) ? 0: parseInt(req.query.b);
+  const result = a + b;
+
+  res.json({
+    result: result
+  })
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
+
+
